426e40dfbc16484a2368e69e0286c57a56a36cc0
Deaglan Bartlett <deaglanbartlett@admins-MacBook-Pro.local>
2023-03-04 16:23:46 +0100

    Print equation in scripts/process_equation_file.py

--- scripts/process_equation_file.py
+++ scripts/process_equation_file.py
@@ -10,38 +10,45 @@ import os
 import sys
 from mpi4py import MPI
 
-os.chdir('../ESR/generation')
-sys.path.insert(1, '.')
-import generator
+#os.chdir('../ESR/generation')
+#sys.path.insert(1, '.')
+#import generator
+import esr.generation.generator as generator
 
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 size = comm.Get_size()
 
+dirname = '/Users/deaglanbartlett/Desktop/OneDrive - Nexus365/IAP/Work/Exhaustive_Symbolic_Regression/benchmark/'
+
 basis_functions = [["x", "a"],  # type0
                 ["square", "exp", "inv", "sqrt_abs", "log_abs"],  # type1
                 ["+", "*", "-", "/", "pow"]]  # type2
-data_fname = '/Users/deaglanbartlett/Desktop/benchmark/feynman_I_6_2a.tsv'
+data_fname = dirname + 'feynman_I_6_2a.tsv'
 
-fn_fname = '/Users/deaglanbartlett/Desktop/benchmark/DM_feyn_fcns_5hr_2.dat'
-output_full_fname = '/Users/deaglanbartlett/Desktop/benchmark/DM_5hr_converted.txt'
-output_pareto_fname = '/Users/deaglanbartlett/Desktop/benchmark/DM_5hr_pareto.txt'
+fn_fname = dirname + 'DM_feyn_fcns_5hr_2.dat'
+output_full_fname = dirname + 'DM_5hr_converted.txt'
+output_pareto_fname = dirname + 'DM_5hr_pareto.txt'
+output_pareto_fun_fname = dirname + 'DM_5hr_pareto_fun.txt'
 
-fn_fname = '/Users/deaglanbartlett/Desktop/benchmark/DM_feyn_fcns_10hr_2.dat'
-output_full_fname = '/Users/deaglanbartlett/Desktop/benchmark/DM_10hr_converted.txt'
-output_pareto_fname = '/Users/deaglanbartlett/Desktop/benchmark/DM_10hr_pareto.txt'
+fn_fname = dirname + 'DM_feyn_fcns_10hr_2.dat'
+output_full_fname = dirname + 'DM_10hr_converted.txt'
+output_pareto_fname = dirname + 'DM_10hr_pareto.txt'
+output_pareto_fun_fname = dirname + 'DM_10hr_pareto_fun.txt'
 
-fn_fname = '/Users/deaglanbartlett/Desktop/benchmark/qlattice_BIC.dat'
-output_full_fname = '/Users/deaglanbartlett/Desktop/benchmark/qlattice_BIC_converted.txt'
-output_pareto_fname = '/Users/deaglanbartlett/Desktop/benchmark/qlattice_BIC_pareto.txt'
-#
-fn_fname = '/Users/deaglanbartlett/Desktop/benchmark/qlattice_MSE.dat'
-output_full_fname = '/Users/deaglanbartlett/Desktop/benchmark/qlattice_MSE_converted.txt'
-output_pareto_fname = '/Users/deaglanbartlett/Desktop/benchmark/qlattice_MSE_pareto.txt'
+fn_fname = dirname + 'qlattice_BIC.dat'
+output_full_fname = dirname + 'qlattice_BIC_converted.txt'
+output_pareto_fname = dirname + 'qlattice_BIC_pareto.txt'
+output_pareto_fun_fname = dirname + 'qlattice_BIC_pareto_fun.txt'
+
+
+fn_fname = dirname + 'qlattice_MSE.dat'
+output_full_fname = dirname + 'qlattice_MSE_converted.txt'
+output_pareto_fname = dirname + 'qlattice_MSE_pareto.txt'
+output_pareto_fun_fname = dirname + 'qlattice_MSE_pareto_fun.txt'
 
 with open(fn_fname, 'r') as f:
     all_fun = f.read().splitlines()
-#all_fun = all_fun[:20]
 
 df = np.array(pd.read_table(data_fname, sep="\t"))
 xtrue = df[:,0]
@@ -84,25 +91,38 @@ else:
 compl = comm.bcast(compl, root=0)
 mse = comm.bcast(mse, root=0)
 
+compl = np.array(compl)
+mse = np.array(mse)
+
 if rank == 0:
     np.savetxt(output_full_fname, np.transpose(np.vstack([compl, mse])))
     
     # Make Pareto front
     unique_compl = np.unique(compl)
     pareto_mse = np.empty(len(unique_compl))
+    pareto_fun = [None] * len(unique_compl)
     mse = np.array(mse)
     
     for i, c in enumerate(unique_compl):
         m = compl == c
         if m.sum() == 1:
             pareto_mse[i] = np.squeeze(mse[m])
+            j = np.squeeze(np.where(m == 1))
         else:
             pareto_mse[i] = np.amin(mse[m])
+            j = np.squeeze(np.where(mse == pareto_mse[i]))
+            if isinstance(j, np.ndarray):
+                j = j[compl[j]==c][0]
+        pareto_fun[i] = all_fun[j]
+        
     np.savetxt(output_pareto_fname, np.transpose(np.vstack([unique_compl, pareto_mse])))
     
+    with open(output_pareto_fun_fname, 'w') as f:
+        for i in range(len(unique_compl)):
+            print(unique_compl[i], pareto_mse[i], '(' + pareto_fun[i] + ')', file=f)
     
     i = np.argmin(mse)
-    print(compl[i], mse[i])
+    print(compl[i], mse[i], all_fun[i])
         
     
     
