ff5254c8584aeb4d95474a623baebcc733466423
Deaglan Bartlett <deaglanbartlett@admins-MacBook-Pro.local>
2023-04-21 11:55:57 +0200

    Check and make function_library

--- esr/generation/duplicate_checker.py
+++ esr/generation/duplicate_checker.py
@@ -50,7 +50,12 @@ def main(runname, compl, track_memory=False, search_tmax=60, expand_tmax=1, seed
                 ["inv", "sin"],  # type1
                 ["+", "*", "-", "/", "pow"]]  # type2
 
-    dirname = os.path.abspath(os.path.join(os.path.dirname(generator.__file__), '..', 'function_library')) + '/' + runname + '/'
+    dirname = os.path.abspath(os.path.join(os.path.dirname(generator.__file__), '..', 'function_library'))
+    if not os.path.isdir(dirname):
+        os.mkdir(dirname)
+    dirname += '/' + runname + '/'
+    if not os.path.isdir(dirname):
+        os.mkdir(dirname)
     
     if (rank == 0) and (not os.path.isdir(dirname)):
         print('Making output directory:', dirname)
