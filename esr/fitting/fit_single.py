3e5538b50faab2eaa28f9f914fa99726c9cc7008
DeaglanBartlett <47668431+DeaglanBartlett@users.noreply.github.com>
2023-07-27 17:27:33 +0200

    Allow fitting of function from a string

--- esr/fitting/fit_single.py
+++ esr/fitting/fit_single.py
@@ -6,6 +6,22 @@ from esr.fitting.test_all_Fisher import convert_params
 import esr.generation.generator as generator
 import esr.generation.simplifier as simplifier
 
+def is_float(string):
+    """Determine whether a string is a float or not
+    
+    Args:
+        :string (str): The string to check
+        
+    Returns:
+        bool: Whether the string is a float (True) or not (False).
+    
+    """
+    try:
+        float(string)
+        return True
+    except ValueError:
+        return False
+
 def single_function(labels, basis_functions, likelihood, pmin=0, pmax=5, tmax=5, try_integration=False, verbose=False, Niter=30, Nconv=5):
     """Run end-to-end fitting of function for a single function
     
@@ -74,4 +90,74 @@ def single_function(labels, basis_functions, likelihood, pmin=0, pmax=5, tmax=5,
             print('\nDescription length:', DL)
 
     return negloglike, DL
+    
+    
+def fit_from_string(fun, basis_functions, likelihood, pmin=0, pmax=5, tmax=5, try_integration=False, verbose=False, Niter=30, Nconv=5, maxvar=20):
+    """Run end-to-end fitting of function for a single function, given as a string. Note that this is not guaranteed to find the optimimum representation as a tree, so there could be a lower description-length representation of the function
+    
+    Args:
+        :fun (str): String representation of the function to be fitted
+        :basis_functions (list): list of lists basis functions. basis_functions[0] are nullary, basis_functions[1] are unary and basis_functions[2] are binary operators
+        :likelihood (fitting.likelihood object): object containing data, likelihood functions and file paths
+        :pmin (float, default=0.): minimum value for each parameter to consider when generating initial guess
+        :pmax (float, default=3.): maximum value for each parameter to consider when generating initial guess
+        :tmax (float, default=5.): maximum time in seconds to run any one part of simplification procedure for a given function
+        :try_integration (bool, default=False): when likelihood requires integral, whether to try to analytically integrate (True) or just numerically integrate (False)
+        :verbose (bool, default=True): Whether to print results (True) or not (False)
+        :Niter (int, default=30): Maximum number of parameter optimisation iterations to attempt.
+        :Nconv (int, default=5): If we find Nconv solutions for the parameters which are within a logL of 0.5 of the best, we say we have converged and stop optimising parameters
+        :maxvar (int): The maximum number of variables which could appear in the function
+    
+    Returns:
+         :negloglike (float): the minimum value of -log(likelihood) (corresponding to the maximum likelihood)
+         :DL (float): the description length of this function
+         :labels (list): list of strings giving node labels of tree
+    
+    """
+
+    expr, nodes, complexity = generator.string_to_node(fun, basis_functions, evalf=True)
+    labels = nodes.to_list(basis_functions)
+    
+    # Prepare to get parents
+    new_labels = [None] * len(labels)
+    for j, lab in enumerate(labels):
+        if lab == 'Mul':
+            new_labels[j] = '*'
+            labels[j] = '*'
+        elif lab == 'Add':
+            new_labels[j] = '+'
+            labels[j] = '+'
+        else:
+            new_labels[j] = lab.lower()
+            labels[j] = lab.lower()
+    param_idx = [j for j, lab in enumerate(new_labels) if is_float(lab)]
+    assert len(param_idx) <= maxvar
+    for k, j in enumerate(param_idx):
+        new_labels[j] = f'a{k}'
+
+    # Get parent operators
+    s = generator.labels_to_shape(new_labels, basis_functions)
+    success, _, tree = generator.check_tree(s)
+    parents = [None] + [labels[p.parent] for p in tree[1:]]
+    
+    # Replace floats with symbols (except exponents)
+    param_idx = [j for j, lab in enumerate(labels) if is_float(lab) and not (parents[j].lower() =='pow')]
+    for k, j in enumerate(param_idx):
+        labels[j] = f'a{k}'
+    fstr = generator.node_to_string(0, tree, labels)
+    print(labels)
+    negloglike, DL = single_function(
+            labels,
+            basis_functions,
+            likelihood,
+            pmin=pmin,
+            pmax=pmax,
+            tmax=tmax,
+            try_integration=try_integration,
+            verbose=verbose,
+            Niter=Niter,
+            Nconv=Nconv
+    )
+
+    return negloglike, DL, labels
 
