e57936f94b80c7ae564fa1919cb66a5fc266e73b
DeaglanBartlett <47668431+DeaglanBartlett@users.noreply.github.com>
2024-01-31 10:48:55 +0100

    Add string_to_aifeyn and tree_to_aifeyn functions in fit_single

--- esr/fitting/fit_single.py
+++ esr/fitting/fit_single.py
@@ -207,4 +207,101 @@ def fit_from_string(fun, basis_functions, likelihood, pmin=0, pmax=5, tmax=5,
         return res[0], res[1], labels, res[2]
     
     return res[0], res[1], labels
+    
+    
+def tree_to_aifeyn(labels, basis_functions, verbose=True):
+    """
+    Takes a list of labels defining a function and returns the AIFeyn term of
+    complexity and the complexity of the function
+    
+    Args:
+        :labels (list): list of strings giving node labels of tree
+        :basis_functions (list): list of lists basis functions. basis_functions[0] are
+            nullary, basis_functions[1] are unary and basis_functions[2] are
+            binary operators
+        :verbose (bool, default=True): Whether to print results (True) or not (False)
+    
+    Returns:
+        :aifeyn (float): the contribution to description length from describing tree
+        :complexity (int): the number of nodes in the function
+    """
+
+    # Convert the string to a sympy function
+    s = generator.labels_to_shape(labels, basis_functions)
+    success, _, tree = generator.check_tree(s)
+    fstr = generator.node_to_string(0, tree, labels)
+    max_param = simplifier.get_max_param([fstr], verbose=verbose)
+    
+    # Get the functional complexity
+    param_list = ['a%i'%j for j in range(max_param)]
+    aifeyn = generator.aifeyn_complexity(labels, param_list)
+    if verbose:
+        print('Function:', aifeyn)
+
+    return aifeyn, len(labels)
+    
+    
+def string_to_aifeyn(fun, basis_functions, maxvar=20, verbose=True,
+    replace_floats=False):
+    """
+    Takes a string defining a function and returns the AIFeyn term of
+    complexity and the complexity of the function
+    
+    Args:
+        :fun (str): String representation of the function to be fitted
+        :basis_functions (list): list of lists basis functions. basis_functions[0] are
+            nullary, basis_functions[1] are unary and basis_functions[2] are
+            binary operators
+        :maxvar (int, default=20): The maximum number of variables which could appear
+            in the function
+        :verbose (bool, default=True): Whether to print results (True) or not (False)
+        :replace_floats (bool, default=False): whether to replace any numbers found in
+            the function with variables to optimise
+    
+    Returns:
+        :aifeyn (float): the contribution to description length from describing tree
+        :complexity (int): the number of nodes in the function
+    """
+    
+    expr, nodes, complexity = generator.string_to_node(fun, basis_functions, evalf=True)
+    labels = nodes.to_list(basis_functions)
+    
+    # Prepare to get parents
+    new_labels = [None] * len(labels)
+    for j, lab in enumerate(labels):
+        if lab == 'Mul':
+            new_labels[j] = '*'
+            labels[j] = '*'
+        elif lab == 'Add':
+            new_labels[j] = '+'
+            labels[j] = '+'
+        elif lab == 'Div':
+            new_labels[j] = '/'
+            labels[j] = '/'
+        elif lab == 'Sub':
+            new_labels[j] = '-'
+            labels[j] = '-'
+        else:
+            new_labels[j] = lab.lower()
+            labels[j] = lab.lower()
+    param_idx = [j for j, lab in enumerate(new_labels) if generator.is_float(lab) or (lab.startswith('a') and generator.is_float(lab[1:]))]
+    assert len(param_idx) <= maxvar
+    for k, j in enumerate(param_idx):
+        new_labels[j] = f'a{k}'
+        
+    # Get parent operators
+    s = generator.labels_to_shape(new_labels, basis_functions)
+    success, _, tree = generator.check_tree(s)
+    parents = [None] + [labels[p.parent] for p in tree[1:]]
+    
+    # Replace floats with symbols (except exponents)
+    if replace_floats:
+        param_idx = [j for j, lab in enumerate(labels) if (generator.is_float(lab) and not (parents[j].lower() =='pow')) or (lab.startswith('a') and generator.is_float(lab[1:]))]
+        for k, j in enumerate(param_idx):
+            labels[j] = f'a{k}'
+    fstr = generator.node_to_string(0, tree, labels)
+    print(labels)
+
+    return tree_to_aifeyn(labels, basis_functions, verbose=verbose)
+    
 
