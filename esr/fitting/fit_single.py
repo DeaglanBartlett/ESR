6a91045a02ae67e295a45581874b20c83e8487c8
DeaglanBartlett <47668431+DeaglanBartlett@users.noreply.github.com>
2024-01-31 10:36:03 +0100

    Add return_params argument to fit_single functions

--- esr/fitting/fit_single.py
+++ esr/fitting/fit_single.py
@@ -6,25 +6,44 @@ from esr.fitting.test_all_Fisher import convert_params
 import esr.generation.generator as generator
 import esr.generation.simplifier as simplifier
 
-def single_function(labels, basis_functions, likelihood, pmin=0, pmax=5, tmax=5, try_integration=False, verbose=False, Niter=30, Nconv=5, log_opt=False):
+def single_function(labels, basis_functions, likelihood, pmin=0, pmax=5, tmax=5,
+    try_integration=False, verbose=False, Niter=30, Nconv=5, log_opt=False,
+    return_params=False):
     """Run end-to-end fitting of function for a single function
     
     Args:
         :labels (list): list of strings giving node labels of tree
-        :basis_functions (list): list of lists basis functions. basis_functions[0] are nullary, basis_functions[1] are unary and basis_functions[2] are binary operators
-        :likelihood (fitting.likelihood object): object containing data, likelihood functions and file paths
-        :pmin (float, default=0.): minimum value for each parameter to consider when generating initial guess
-        :pmax (float, default=3.): maximum value for each parameter to consider when generating initial guess
-        :tmax (float, default=5.): maximum time in seconds to run any one part of simplification procedure for a given function
-        :try_integration (bool, default=False): when likelihood requires integral, whether to try to analytically integrate (True) or just numerically integrate (False)
+        :basis_functions (list): list of lists basis functions. basis_functions[0] are
+            nullary, basis_functions[1] are unary and basis_functions[2] are
+            binary operators
+        :likelihood (fitting.likelihood object): object containing data, likelihood
+            functions and file paths
+        :pmin (float, default=0.): minimum value for each parameter to consider when
+            generating initial guess
+        :pmax (float, default=3.): maximum value for each parameter to consider when
+            generating initial guess
+        :tmax (float, default=5.): maximum time in seconds to run any one part of
+            simplification procedure for a given function
+        :try_integration (bool, default=False): when likelihood requires integral,
+            whether to try to analytically integrate (True) or just numerically
+            integrate (False)
         :verbose (bool, default=True): Whether to print results (True) or not (False)
-        :Niter (int, default=30): Maximum number of parameter optimisation iterations to attempt.
-        :Nconv (int, default=5): If we find Nconv solutions for the parameters which are within a logL of 0.5 of the best, we say we have converged and stop optimising parameters
-        :log_opt (bool, default=False): whether to optimise 1 and 2 parameter cases in log space
+        :Niter (int, default=30): Maximum number of parameter optimisation iterations
+            to attempt.
+        :Nconv (int, default=5): If we find Nconv solutions for the parameters which are
+            within a logL of 0.5 of the best, we say we have converged and stop
+            optimising parameters
+        :log_opt (bool, default=False): whether to optimise 1 and 2 parameter cases in
+            log space
+        :return_params (bool, default=False): whether to return the parameters of the
+            maximum likelihood point
     
     Returns:
-         :negloglike (float): the minimum value of -log(likelihood) (corresponding to the maximum likelihood)
+         :negloglike (float): the minimum value of -log(likelihood) (corresponding to
+            the maximum likelihood)
          :DL (float): the description length of this function
+         :params (optional, list): the maximum likelihood parameters. Only returned if
+            `return_params` is true
     
     """
 
@@ -33,7 +52,8 @@ def single_function(labels, basis_functions, likelihood, pmin=0, pmax=5, tmax=5,
     success, _, tree = generator.check_tree(s)
     fstr = generator.node_to_string(0, tree, labels)
     max_param = simplifier.get_max_param([fstr], verbose=verbose)
-    fstr, fsym = simplifier.initial_sympify([fstr], max_param, parallel=False, verbose=verbose)
+    fstr, fsym = simplifier.initial_sympify(
+        [fstr], max_param, parallel=False, verbose=verbose)
     fstr = fstr[0]
     fsym = fsym[fstr]
     print(fstr)
@@ -58,7 +78,8 @@ def single_function(labels, basis_functions, likelihood, pmin=0, pmax=5, tmax=5,
         fcn, eq, integrated = likelihood.run_sympify(fstr,
                                                 tmax=tmax,
                                                 try_integration=try_integration)
-        params, negloglike, deriv, codelen = convert_params(fcn, eq, integrated, params, likelihood, chi2, max_param=max_param)
+        params, negloglike, deriv, codelen = convert_params(
+            fcn, eq, integrated, params, likelihood, chi2, max_param=max_param)
         if verbose:
             print('\ntheta_ML:', params)
             print('Residuals:', negloglike, chi2)
@@ -74,32 +95,58 @@ def single_function(labels, basis_functions, likelihood, pmin=0, pmax=5, tmax=5,
         DL = negloglike + codelen + aifeyn
         if verbose:
             print('\nDescription length:', DL)
+            
+    if return_params:
+        return negloglike, DL, params
 
     return negloglike, DL
     
     
-def fit_from_string(fun, basis_functions, likelihood, pmin=0, pmax=5, tmax=5, try_integration=False, verbose=False, Niter=30, Nconv=5, maxvar=20, log_opt=False, replace_floats=False):
-    """Run end-to-end fitting of function for a single function, given as a string. Note that this is not guaranteed to find the optimimum representation as a tree, so there could be a lower description-length representation of the function
+def fit_from_string(fun, basis_functions, likelihood, pmin=0, pmax=5, tmax=5,
+    try_integration=False, verbose=False, Niter=30, Nconv=5, maxvar=20,
+    log_opt=False, replace_floats=False, return_params=False):
+    """Run end-to-end fitting of function for a single function, given as a string.
+    Note that this is not guaranteed to find the optimimum representation as a tree,
+    so there could be a lower description-length representation of the function
     
     Args:
         :fun (str): String representation of the function to be fitted
-        :basis_functions (list): list of lists basis functions. basis_functions[0] are nullary, basis_functions[1] are unary and basis_functions[2] are binary operators
-        :likelihood (fitting.likelihood object): object containing data, likelihood functions and file paths
-        :pmin (float, default=0.): minimum value for each parameter to consider when generating initial guess
-        :pmax (float, default=3.): maximum value for each parameter to consider when generating initial guess
-        :tmax (float, default=5.): maximum time in seconds to run any one part of simplification procedure for a given function
-        :try_integration (bool, default=False): when likelihood requires integral, whether to try to analytically integrate (True) or just numerically integrate (False)
+        :basis_functions (list): list of lists basis functions. basis_functions[0] are
+            nullary, basis_functions[1] are unary and basis_functions[2] are binary
+            operators
+        :likelihood (fitting.likelihood object): object containing data, likelihood
+            functions and file paths
+        :pmin (float, default=0.): minimum value for each parameter to consider when
+            generating initial guess
+        :pmax (float, default=3.): maximum value for each parameter to consider when
+            generating initial guess
+        :tmax (float, default=5.): maximum time in seconds to run any one part of
+            simplification procedure for a given function
+        :try_integration (bool, default=False): when likelihood requires integral,
+            whether to try to analytically integrate (True) or just numerically
+            integrate (False)
         :verbose (bool, default=True): Whether to print results (True) or not (False)
-        :Niter (int, default=30): Maximum number of parameter optimisation iterations to attempt.
-        :Nconv (int, default=5): If we find Nconv solutions for the parameters which are within a logL of 0.5 of the best, we say we have converged and stop optimising parameters
-        :maxvar (int): The maximum number of variables which could appear in the function
-        :log_opt (bool, default=False): whether to optimise 1 and 2 parameter cases in log space
-        :replace_floats (bool, default=False): whether to replace any numbers found in the function with variables to optimise
+        :Niter (int, default=30): Maximum number of parameter optimisation iterations
+            to attempt.
+        :Nconv (int, default=5): If we find Nconv solutions for the parameters which
+            are within a logL of 0.5 of the best, we say we have converged and stop
+            optimising parameters
+        :maxvar (int): The maximum number of variables which could appear in the
+            function
+        :log_opt (bool, default=False): whether to optimise 1 and 2 parameter cases in
+            log space
+        :replace_floats (bool, default=False): whether to replace any numbers found in
+            the function with variables to optimise
+        :return_params (bool, default=False): whether to return the parameters of the
+            maximum likelihood point
     
     Returns:
-         :negloglike (float): the minimum value of -log(likelihood) (corresponding to the maximum likelihood)
+         :negloglike (float): the minimum value of -log(likelihood) (corresponding to
+            the maximum likelihood)
          :DL (float): the description length of this function
          :labels (list): list of strings giving node labels of tree
+         :params (optional, list): the maximum likelihood parameters. Only returned if
+            `return_params` is true
     
     """
 
@@ -141,7 +188,7 @@ def fit_from_string(fun, basis_functions, likelihood, pmin=0, pmax=5, tmax=5, tr
             labels[j] = f'a{k}'
     fstr = generator.node_to_string(0, tree, labels)
     print(labels)
-    negloglike, DL = single_function(
+    res = single_function(
             labels,
             basis_functions,
             likelihood,
@@ -153,7 +200,11 @@ def fit_from_string(fun, basis_functions, likelihood, pmin=0, pmax=5, tmax=5, tr
             Niter=Niter,
             Nconv=Nconv,
             log_opt=log_opt,
+            return_params=return_params
     )
-
-    return negloglike, DL, labels
+    
+    if return_params:
+        return res[0], res[1], labels, res[2]
+    
+    return res[0], res[1], labels
 
